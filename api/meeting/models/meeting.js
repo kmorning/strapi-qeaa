87a4a381f3dd6236ae405adb4105cd86685ea36a
Kelly Morning <kleemorning@gmail.com>
2021-02-15 00:11:56 -0500

    set slug on create/update

--- api/meeting/models/meeting.js
+++ api/meeting/models/meeting.js
@@ -2,6 +2,13 @@
 
 const slugify = require('slugify')
 
+const setSlug = (data) => {
+    if (data.name && data.day && data.time) {
+        const s = data.name + `-` + data.day.toString() + `-` + data.time.toString()
+        data.slug = slugify(s)
+    }
+}
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
@@ -10,10 +17,10 @@ const slugify = require('slugify')
 module.exports = {
     lifecycles: {
         beforeCreate: async (data) => {
-            if (data.name && data.day && data.time) {
-                const s = data.name + `-` + data.day.toString() + `-` + data.time.toString()
-                data.slug = slugify(s)
-            }
+            setSlug(data)
+        },
+        beforeUpdate: async (params, data) => {
+            setSlug(data)
         }
     }
 };
