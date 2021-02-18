b7076e53ad5593078bb970aecbacb27f9a03adef
Kelly Morning <kleemorning@gmail.com>
2021-02-18 08:51:58 -0500

    implement geocoding

--- api/location/models/location.js
+++ api/location/models/location.js
@@ -5,4 +5,32 @@
  * to customize this model
  */
 
-module.exports = {};
+ const geocode = async (data) => {
+    const full_address = ['address', 'city', 'postal_code', 'country'].filter( key => {
+        if (data[key]) {
+            return data[key].length > 0
+        }
+        return false
+    }).map( key => {
+        return data[key]
+    }).join(", ")
+    console.log(full_address)
+    try {
+        const locations = await strapi.services.location.geosearch(full_address)
+        data.longitude = parseFloat(locations[0].lon)
+        data.latitude = parseFloat(locations[0].lat)
+    } catch {
+        return
+    }
+ }
+
+module.exports = {
+    lifecycles: {
+        beforeCreate: async (data) => {
+            await geocode(data)
+        },
+        beforeUpdate: async (params, data) => {
+            await geocode(data)
+        }
+    }
+};
