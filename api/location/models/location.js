8ba7e6a32cb2c9994a2c2a1a916de293fd38a2d1
Kelly Morning <kleemorning@gmail.com>
2021-02-20 14:37:56 -0500

    auto populate field from geocode results

--- api/location/models/location.js
+++ api/location/models/location.js
@@ -5,7 +5,6 @@
  * to customize this model
  */
 
- // TODO: auto populate missing fields like postal code
  const update = async (data) => {
     const search_address = ['name', 'address', 'city', 'province', 'postal_code', 'country'].filter( key => {
         if (data[key]) {
@@ -15,17 +14,73 @@
     }).map( key => {
         return data[key]
     }).join(", ")
+
     //console.log(formatted_address)
-    if (data.search_address !== search_address | data.longitude == null | data.latitude == null) {
+    var location
+    if (data.geocoded_address !== search_address | data.longitude == null | data.latitude == null) {
         try {
             const locations = await strapi.services.location.geosearch(search_address, "1")
+            location = locations[0]
             console.log(locations)
-            data.longitude = parseFloat(locations[0].lon)
-            data.latitude = parseFloat(locations[0].lat)
+            //data.longitude = parseFloat(locations[0].lon)
+            //data.latitude = parseFloat(locations[0].lat)
         } catch {
-            return
+            location = null
         }
-        data.formatted_address = search_address
+        //data.formatted_address = search_address
+    }
+
+    if (location) {
+        const empty_fields = ['name', 'address', 'city', 'postal_code'].filter( key => {
+            if (data[key]) {
+                return data[key].length < 3
+            }
+            return true
+        })
+
+        var house_number
+        var road
+        for (var key in location.address) {
+            switch (key) {
+                case ['place', 'ameninty', 'office', 'club', 'house_name'].includes(key):
+                    if (empty_fields.includes('name')) {
+                        data.name = location.address[key]
+                    }
+                    break
+                case 'house_number':
+                    house_number = location.address[key]
+                    break
+                case 'road':
+                    road = location.address[key]
+                case 'postcode':
+                    if (empty_fields.includes('postal_code')) {
+                        data.postal_code = location.address[key]
+                    }
+                    break
+            }
+        }
+
+        const address = [house_number, road].filter( item => {return item != null}).join(" ")
+
+        if (empty_fields.includes['address']) {
+            data.address = address
+        } else if (house_number) {
+            if (!data.address.startsWith(house_number)) {
+                data.address = address
+            }
+        }
+
+        data.longitude = parseFloat(location.lon)
+        data.latitude = parseFloat(location.lat)
+
+        data.geocoded_address =  ['name', 'address', 'city', 'province', 'postal_code', 'country'].filter( key => {
+            if (data[key]) {
+                return data[key].length > 0
+            }
+            return false
+        }).map( key => {
+            return data[key]
+        }).join(", ")
     }
  }
 
@@ -35,7 +90,7 @@ module.exports = {
             await update(data)
         },
         beforeUpdate: async (params, data) => {
-            console.log(data)
+            //console.log(data)
             await update(data)
         }
     }
