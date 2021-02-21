25d5f5751c35069664cab07c401b7f7f622bb688
Kelly Morning <kleemorning@gmail.com>
2021-02-21 15:12:11 -0500

    change slug to name-id instead of uuid

--- api/meeting/models/meeting.js
+++ api/meeting/models/meeting.js
@@ -1,15 +1,17 @@
 'use strict'
 
-const { v4: uuidv4 } = require('uuid')
+const slugify = require('slugify')
 
-const setSlug = (data) => {
-  //console.log(data)
-  if (data.slug) {
-    if (data.slug.length === 36) {
-      return
+const setSlug = (params, data) => {
+  if (data.name) {
+    const name_slug = slugify(data.name, { lower: true })
+    if (data.slug) {
+      if (data.slug.startsWith(name_slug)) {
+        return
+      }
     }
+    data.slug = name_slug + `-` + params.id.toString()
   }
-  data.slug = uuidv4()
 }
 
 const validateTypes = async (data) => {
@@ -18,17 +20,19 @@ const validateTypes = async (data) => {
     return 'Meeting must have a location'
   }
   // validate unique type categories
-  let unique_catagories = await strapi.query('meeting-type-category').find({ unique: true })
+  if (data.types) {
+    let unique_catagories = await strapi.query('meeting-type-category').find({ unique: true })
 
-  for (var category in unique_catagories) {
-    let id = category.id
-    let types = await strapi.query('meeting-type').find({ category: id })
-    let ids = types.map((type) => type.id)
-    let found_ids = data.types.filter((type) => ids.includes(type))
-    if (found_ids.length > 1) {
-      let found_types = types.filter((type) => found_ids.includes(type.id))
-      let found_details = found_types.map((type) => type.detail)
-      return 'Meeting can only have one of the selected types: ' + found_details
+    for (var category in unique_catagories) {
+      let id = category.id
+      let types = await strapi.query('meeting-type').find({ category: id })
+      let ids = types.map((type) => type.id)
+      let found_ids = data.types.filter((type) => ids.includes(type))
+      if (found_ids.length > 1) {
+        let found_types = types.filter((type) => found_ids.includes(type.id))
+        let found_details = found_types.map((type) => type.detail)
+        return 'Meeting can only have one of the selected types: ' + found_details
+      }
     }
   }
   return null
@@ -42,14 +46,20 @@ const validateTypes = async (data) => {
 module.exports = {
   lifecycles: {
     beforeCreate: async (data) => {
-      setSlug(data)
+      //setSlug(data)
       const err = await validateTypes(data)
       if (err) {
         throw strapi.errors.badRequest(err)
       }
     },
+
+    afterCreate: async (result, data) => {
+      // Trigger update to set slug
+      await strapi.query('meeting').update({ id: result.id }, data)
+    },
     beforeUpdate: async (params, data) => {
-      setSlug(data)
+      setSlug(params, data)
+      //console.log(params)
       const err = await validateTypes(data)
       if (err) {
         throw strapi.errors.badRequest(err)
